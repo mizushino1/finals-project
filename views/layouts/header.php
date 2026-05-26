7af1cf305494df66697524519c282e80fb6275d1
mizushino1 <jolosantos098@gmail.com>
2026-05-26 21:38:58 +0800

    seperated header and footer css from main

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -8,7 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/glass.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/header.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/main.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/footer.css">
 </head>
 
 <body>
