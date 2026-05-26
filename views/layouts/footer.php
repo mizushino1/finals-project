f39f049d2ba4636f484967d4e0c3ffb2f59b7544
mizushino1 <jolosantos098@gmail.com>
2026-05-26 20:49:18 +0800

    added links for developers

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,50 +1,67 @@
-
 <footer class="site-footer">
-        <div class="container-fluid px-5">
-            <div class="row">
+    <div class="container-fluid px-5">
+        <div class="row">
 
-                <!-- Brand -->
-                <div class="col-12 col-md-4 mb-4 mb-md-0">
-                    <img src="./public/img/logo.svg" alt="Artovia" class="footer-logo">
-                    <p class="footer-tagline">Web Development Finals Project</p>
-                </div>
-
-                <!-- Navigate -->
-                <div class="col-6 col-md-3 mb-4 mb-md-0">
-                    <p class="footer-heading">Navigate</p>
-                    <ul class="footer-links">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Commissions</a></li>
-                        <li><a href="#">Artists</a></li>
-                    </ul>
-                </div>
-
-                <!-- Developers -->
-                <div class="col-6 col-md-5">
-                    <p class="footer-heading">Developers</p>
-                    <ul class="footer-devs">
-                        <li>@mizushino1</li>
-                        <li>@gregoryy-web</li>
-                        <li>@Byokuu</li>
-                        <li>@Draven89</li>
-                        <li>@ashlea1430</li>
-                        <li>@jrumandap</li>
-                    </ul>
-                </div>
-
+            <!-- Brand -->
+            <div class="col-12 col-md-4 mb-4 mb-md-0">
+                <img src="./public/img/logo.svg" alt="Artovia" class="footer-logo">
+                <p class="footer-tagline">Web Development Finals Project</p>
             </div>
 
-            <!-- Bottom bar -->
-            <div class="footer-bottom">
-                <p>© 2026 Artovia · Built for Web Development course</p>
+            <!-- Navigate -->
+            <div class="col-6 col-md-3 mb-4 mb-md-0">
+                <p class="footer-heading">Navigate</p>
+                <ul class="footer-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Commissions</a></li>
+                    <li><a href="#">Artists</a></li>
+                </ul>
             </div>
+
+            <!-- Developers -->
+            <div class="col-6 col-md-5">
+                <p class="footer-heading">Developers</p>
+                <ul class="footer-devs">
+                    <li>
+                        <a href="https://github.com/mizushino1" target="_blank" rel="noopener noreferrer"
+                            class="footer-link developer-tag">@mizushino1</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/gregoryy-web" target="_blank" rel="noopener noreferrer"
+                            class="footer-link developer-tag">@gregoryy-web</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Byokuu" target="_blank" rel="noopener noreferrer"
+                            class="footer-link developer-tag">@Byokuu</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Draven89" target="_blank" rel="noopener noreferrer"
+                            class="footer-link developer-tag">@Draven89</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/ashlea1430" target="_blank" rel="noopener noreferrer"
+                            class="footer-link developer-tag">@ashlea1430</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/jrumandap" target="_blank" rel="noopener noreferrer"
+                            class="footer-link developer-tag">@jrumandap</a>
+                    </li>
+                </ul>
+            </div>
+
         </div>
-    </footer>
+
+        <!-- Bottom bar -->
+        <div class="footer-bottom">
+            <p>© 2026 Artovia · Built for Web Development course</p>
+        </div>
+    </div>
+</footer>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
