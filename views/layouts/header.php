ff0c5c505cb611ca18b391d940cfa07cd128ce27
mizushino1 <jolosantos098@gmail.com>
2026-05-27 21:21:02 +0800

    fixed not visibile bootstrap icons

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -8,20 +8,21 @@
     <link href="https://fonts.googleapis.com/css2?family=Joan&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/glass.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/header.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/main.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/footer.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/login.css">
-
 </head>
 
 <body>
     <header class="sticky-top">
-        <nav class="navbar navbar-expand-md" style="background-color: #111112;">
+        <nav class="navbar navbar-expand-md site-navbar">
             <div class="container-fluid">
-                <a class="navbar-brand ms-3" href="#"><img src="<?php echo BASE_URL; ?>public/img/logo.svg" alt=""
-                        style="filter:invert(1);height:2rem"></a>
+                <a class="navbar-brand ms-3" href="#">
+                    <img src="<?php echo BASE_URL; ?>public/img/logo.svg" alt="" class="navbar-logo">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -37,8 +38,7 @@
                     </div>
                     <div class="navbar-nav">
                         <a class="btn btn-primary glass-card fs-fluid-xs"
-                            href="<?php echo BASE_URL; ?>login">Log in/ Sign
-                            up</a>
+                            href="<?php echo BASE_URL; ?>login">Log in / Sign up</a>
                     </div>
                 </div>
             </div>
