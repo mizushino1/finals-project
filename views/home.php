4ab087658a54af85a2b0d10c89839e90dfc9599a
Byokuu <coyunesbyorne@gmail.com>
2026-05-27 11:09:59 +0800

    home.php bg

---
+++ views/home.php
@@ -0,0 +1,8 @@
+<main class="">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col home-bg" style="height:100vh">
+            </div>
+        </div>
+    </div>
+</main>
