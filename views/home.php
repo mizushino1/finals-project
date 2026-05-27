9b7093d67aa26c057dc522e1ee1bb428c1321c9f
Draven89 <alexis2.berbon@gmail.com>
2026-05-27 13:52:04 +0800

    more home page design

--- views/home.php
+++ views/home.php
@@ -1,13 +1,64 @@
 <main class="">
     <div class="container-fluid">
         <div class="row">
-            <div class="col home-bg" style="height:100vh">
+            <div class="col home-bg" style="min-height:100vh">
                 <div class="row">
-                    <div class="col">
-                        <h1 class="joan fw-light p-4 mt-5" style="font-size: 96px;">
-                            Design it. <br>
-                            Create it. Get Paid.
-                        </h1>
+                    <div class="col home-bg" style="min-height:100vh">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1 class="joan fw-light px-4 pt-4 mb-0 mt-5" style="font-size: 96px;">
+                                    Design it. <br>
+                                    Create it. Get Paid.
+                                </h1>
+                                <p class="ps-4 ms-1 mb-0 roboto fw-bold fs-fluid-md ">Artovia is a platform where you
+                                    can
+                                    create,
+                                    collaborate and get paid <br> for your creativity
+                                </p>
+                            </div>
+                            <div class="col-12 col-xl-8">
+                                <div class="row">
+                                    <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
+                                        <img src="./public/img/connectIcon.svg" class="me-4">
+                                        <p class="">
+                                            <span class="fs-fluid-xs fw-bold theme-font-color">Design with freedom
+                                            </span><br>
+                                            <span class="fs-fluid-xxs">Design and Create with no limits, make your ideas
+                                                come to life!</span>
+                                        </p>
+
+                                    </div>
+                                    <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
+                                        <img src="./public/img/peopleIcon.svg" class="me-4">
+                                        <p class="">
+                                            <span class="fs-fluid-xs fw-bold theme-font-color">Work and collaborate
+                                            </span><br>
+                                            <span class="fs-fluid-xxs">Connect and collaborate with skilled and talented
+                                                users worldwide.</span>
+                                        </p>
+
+                                    </div>
+                                    <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
+                                        <img src="./public/img/walletIcon.svg" class="me-4">
+                                        <p class="">
+                                            <span class="fs-fluid-xs fw-bold theme-font-color">Get paid securely
+                                            </span><br>
+                                            <span class="fs-fluid-xxs">We make sure, creators get what they
+                                                deserve.</span>
+                                        </p>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col-6">
+
+                            </div>
+
+
+
+                        </div>
+
                     </div>
                 </div>
             </div>
