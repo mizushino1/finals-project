5766adf4c138c834124081320c6041fedb460b25
Draven89 <alexis2.berbon@gmail.com>
2026-05-27 14:28:03 +0800

    icon fix

--- views/home.php
+++ views/home.php
@@ -18,37 +18,31 @@
                             </div>
                             <div class="col-12 col-xl-8">
                                 <div class="row">
-                                    <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
-                                        <img src="./public/img/connectIcon.svg" class="me-4">
-                                        <p class="">
-                                            <span class="fs-fluid-xs fw-bold theme-font-color">Design with freedom
-                                            </span><br>
-                                            <span class="fs-fluid-xxs">Design and Create with no limits, make your ideas
-                                                come to life!</span>
-                                        </p>
+                                <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
+                                    <img src="./public/img/connectIcon.svg" class="me-4 img-fluid">
+                                    <p class="">
+                                        <span class="fs-fluid-xs fw-bold theme-font-color">Design with freedom </span><br>
+                                        <span class="fs-fluid-xxs">Design and Create with no limits, make your ideas come to life!</span>
+                                    </p>
 
-                                    </div>
-                                    <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
-                                        <img src="./public/img/peopleIcon.svg" class="me-4">
-                                        <p class="">
-                                            <span class="fs-fluid-xs fw-bold theme-font-color">Work and collaborate
-                                            </span><br>
-                                            <span class="fs-fluid-xxs">Connect and collaborate with skilled and talented
-                                                users worldwide.</span>
-                                        </p>
-
-                                    </div>
-                                    <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
-                                        <img src="./public/img/walletIcon.svg" class="me-4">
-                                        <p class="">
-                                            <span class="fs-fluid-xs fw-bold theme-font-color">Get paid securely
-                                            </span><br>
-                                            <span class="fs-fluid-xxs">We make sure, creators get what they
-                                                deserve.</span>
-                                        </p>
-
-                                    </div>
                                 </div>
+                                <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
+                                    <img src="./public/img/peopleIcon.svg" class="me-4 img-fluid">
+                                    <p class="">
+                                        <span class="fs-fluid-xs fw-bold theme-font-color">Work and collaborate  </span><br>
+                                        <span class="fs-fluid-xxs">Connect and collaborate with skilled and talented users worldwide.</span>
+                                    </p>
+
+                                </div>
+                                <div class="col-12 col-md-4 ps-5 my-3 d-flex flex-row">
+                                    <img src="./public/img/walletIcon.svg" class="me-4 img-fluid">
+                                    <p class="">
+                                        <span class="fs-fluid-xs fw-bold theme-font-color">Get paid securely </span><br>
+                                        <span class="fs-fluid-xxs">We make sure, creators get what they deserve.</span>
+                                    </p>
+
+                                </div>
+                            </div>
 
                             </div>
                             <div class="col-6">
