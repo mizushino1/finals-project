bb19aa86829da1aeb33e731b247e6fa7d3704619
Draven89 <alexis2.berbon@gmail.com>
2026-05-27 13:28:47 +0800

    fix again

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-    <heade class="sticky-top">
+    <header class="sticky-top">
         <nav class="navbar navbar-expand" style="background-color: #111112;">
             <div class="container-fluid">
                 <a class="navbar-brand ms-3" href="#"><img src="<?php echo BASE_URL; ?>public/img/logo.svg" alt=""
