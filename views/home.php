51b7afafcdf96909b58ccc415366984fae8d120d
Byokuu <coyunesbyorne@gmail.com>
2026-05-27 11:30:31 +0800

    transferred index.html contents to home.php

--- views/home.php
+++ views/home.php
@@ -2,6 +2,14 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col home-bg" style="height:100vh">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="joan fw-light p-4 mt-5" style="font-size: 96px;">
+                            Design it. <br>
+                            Create it. Get Paid.
+                        </h1>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
