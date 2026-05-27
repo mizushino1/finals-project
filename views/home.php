db2d6402b5789a4ca66f70f79d43e246d6f7b380
mizushino1 <jolosantos098@gmail.com>
2026-05-27 20:12:36 +0800

    transferred classes from the index.html to hope.php

--- views/home.php
+++ views/home.php
@@ -6,11 +6,11 @@
                     <div class="col home-bg" style="min-height:100vh">
                         <div class="row">
                             <div class="col-12">
-                                <h1 class="joan fw-light px-4 pt-4 mb-0 mt-5" style="font-size: 96px;">
+                                <h1 class="joan fw-light px-4 pt-4 mb-0 mt-5 header-fluid-home">
                                     Design it. <br>
                                     Create it. Get Paid.
                                 </h1>
-                                <p class="ps-4 ms-1 mb-0 roboto fw-bold fs-fluid-md ">Artovia is a platform where you
+                                <p class="ps-4 ms-1 mb-0 roboto fw-bold fs-fluid-sm ">Artovia is a platform where you
                                     can
                                     create,
                                     collaborate and get paid <br> for your creativity
