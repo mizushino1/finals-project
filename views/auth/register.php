645b265a46846a3e48ce8c8c76bf3f815cf36bbf
gregoryy-web <neilgregorycescleto.14@gmail.com>
2026-05-27 10:51:04 +0800

    index html adjusted

---
+++ views/auth/register.php
@@ -0,0 +1,9 @@
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-light">Hello world</h1>
+            </div>
+        </div>
+    </div>
+</main>
