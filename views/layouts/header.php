30a2b5de7d82e399a5091c38af9f4c88ae5c5973
Draven89 <alexis2.berbon@gmail.com>
2026-05-29 13:26:36 +0800

    gabriel garcia

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>artovia</title>
-    <link rel="icon" type="image/png" href="./public/img/icon.svg">
+    <title>Artovia</title>
+    <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>public/img/icon.svg">
     <link href="https://fonts.googleapis.com/css2?family=Joan&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
