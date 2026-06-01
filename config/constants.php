2da9a45476efebc4d8d5273edd83b0ad8d29e8cf
mizushino1 <jolosantos098@gmail.com>
2026-06-01 21:27:30 +0800

    fix typo

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 define('BASE_URL', 'http://localhost/finals-project/');
 define('DB_HOST',   'localhost');
-define('DB_NAME',   'artovia');
+define('DB_NAME',   'artovia_db');
 define('DB_USER',   'root');
 define('DB_PASS',   'Pogiako123'); //change nyo if ano pass ng database server nyo
 ?>
